bbd2d383416339f1f829a91a4f9a3918beefe4e5
Naila Malik <Naila.Malik@sagacious.local>
2022-10-01 05:02:03 -0700

    leaveReq

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LeaveDetails from "./Screens/LeaveDetails";
+import ApplyForLeave from "./Screens/ApplyForLeave";
+import COLORS from "./Screens/COLOR";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="LeaveDetails"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: COLORS.linearStart,
+          },
+          // headerTintColor: '#fff',
+          // headerTitleStyle: {
+          //   fontWeight: 'bold',
+          // },
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="LeaveDetails" component={LeaveDetails} />
+        <Stack.Screen name="ApplyForLeave" component={ApplyForLeave} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
